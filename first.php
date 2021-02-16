e9f71bac30ce64f11df152fbb2eb865f97a0a802
Akshat Maheshwari <maheshwariakshat482@gmail.com>
2021-02-16 12:10:11 +0530

    added features

--- first.php
+++ first.php
@@ -20,13 +20,56 @@ and open the template in the editor.
         .b-color {
             background-color: #ff1f1f!important;
         }
+        
+.dropbtn {
+  background-color: #4CAF50;
+  color: white;
+  padding: 16px;
+  font-size: 16px;
+  border: none;
+  cursor: pointer;
+}
+
+.dropdown {
+  position: relative;
+  display: inline-block;
+}
+
+.dropdown-content {
+  display: none;
+  position: absolute;
+  background-color: #f9f9f9;
+  min-width: 160px;
+  box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
+  z-index: 1;
+}
+
+.dropdown-content a {
+  color: black;
+  padding: 12px 16px;
+  text-decoration: none;
+  display: block;
+}
+
+.dropdown-content a:hover {background-color: #f1f1f1}
+
+.dropdown:hover .dropdown-content {
+  display: block;
+}
+
+.dropdown:hover .dropbtn {
+  background-color: #3e8e41;
+}
+
+
     </style>
 </head>
 
 <body>
     <div class="m-0 p-0">
         <nav class="navbar navbar-expand-lg navbar-light bg-primary m-0 p-3 shadow">
-            <a class="navbar-brand   text-white" href="./index.php"><span ><img src = "svvv_ninjas.png" style = "height:60px;"></span>Ninjas Freelancing</a>
+            <a class="navbar-brand   text-white" href="./index.php"><span ><img src = "svvv_ninjas.png" style = "height:60px;">
+        </span>Ninjas Freelancing</a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" 
             aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
@@ -52,6 +95,26 @@ and open the template in the editor.
                     </div>
         </nav>
     </div>
+
+<div class="dropdown">
+  <button class="dropbtn">Find Job</button>
+  <div class="dropdown-content">
+  <a href="#">By Skill</a>
+  <a href="#">By Region</a>
+  </div>
+</div>
+
+<div class="dropdown">
+  <button class="dropbtn">Find Freelancers</button>
+  <div class="dropdown-content">
+  <a href="#">By Skill</a>
+  <a href="#">By Region</a>
+  </div>
+</div>
+
+</body>
+</html>
+
     
 </body>
 </html>
